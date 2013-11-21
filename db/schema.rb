1581455ea81a734d8e0bb12e6c77d1a7a76ee15d
Shirokuro <shirokuro0418@gmail.com>
2013-11-21 10:05:04 +0900

    rake db:migrate でのスキーマ追加

--- db/schema.rb
+++ db/schema.rb
@@ -1,14 +1,4 @@
 # encoding: UTF-8
-#---
-# Excerpted from "Agile Web Development with Rails, 4th Ed.",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#
-# 日本語版については http://ssl.ohmsha.co.jp/cgi-bin/menu.cgi?ISBN=978-4-274-06866-9
-#---
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -24,15 +14,15 @@
 ActiveRecord::Schema.define(:version => 20110711000008) do
 
   create_table "carts", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "line_items", :force => true do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
     t.integer  "quantity",   :default => 1
     t.decimal  "price"
     t.integer  "order_id"
@@ -43,8 +33,8 @@ ActiveRecord::Schema.define(:version => 20110711000008) do
     t.text     "address"
     t.string   "email"
     t.string   "pay_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "products", :force => true do |t|
@@ -52,8 +42,8 @@ ActiveRecord::Schema.define(:version => 20110711000008) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       :precision => 8, :scale => 2
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
   end
 
 end
