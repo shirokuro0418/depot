cb1f938025f4af6b77d39388028e079d4da5dff0
Shirokuro <shirokuro0418@gmail.com>
2013-11-22 09:08:50 +0900

    htmlのコメントをerbのコメントに変更

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -20,7 +20,7 @@ class OrderNotifier < ActionMailer::Base
   def received
     @greeting = "Hi"
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
