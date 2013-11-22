79c47813199d9989076abee79947629dfc06d461
Shirokuro <shirokuro0418@gmail.com>
2013-11-22 22:47:53 +0900

    P194:１４章「ログイン」終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :password, :password_confirmation
 
+  after_destroy :ensure_an_admin_remains
+
   validates :name, presence: true, uniqueness: true
   has_secure_password
 end
